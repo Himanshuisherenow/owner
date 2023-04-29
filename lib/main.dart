3d6bb4bb783e244e0f3a30ad91ad9c9532738259
Chavda Himanshu H <himanshuisherenow@gmail.com>
2023-04-30 02:17:11 +0530

    new added

---
+++ lib/main.dart
@@ -0,0 +1,124 @@
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'QR Generator',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('User Details'),
+        ),
+        body: Center(
+          child: QRGenerator(),
+        ),
+      ),
+    );
+  }
+}
+
+class QRGenerator extends StatefulWidget {
+  @override
+  _QRGeneratorState createState() => _QRGeneratorState();
+}
+
+class _QRGeneratorState extends State<QRGenerator> {
+  final _formKey = GlobalKey<FormState>();
+  final _textController1 = TextEditingController();
+  final _textController2 = TextEditingController();
+  final _textController3 = TextEditingController();
+  String _qrData = "";
+
+  @override
+  void dispose() {
+    _textController1.dispose();
+    _textController2.dispose();
+    _textController3.dispose();
+    super.dispose();
+  }
+
+  void _generateQRCode() {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        // Concatenate the three text fields
+        _qrData = "${_textController1.text},${_textController2.text},${_textController3.text}";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextFormField(
+            controller: _textController1,
+            decoration: InputDecoration(
+              labelText: 'Name',
+              hintText: 'Enter your name',
+              border: OutlineInputBorder(),
+            ),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter your name';
+              }
+              return null;
+            },
+          ),
+          SizedBox(height: 16),
+          TextFormField(
+            controller: _textController2,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: 'Phone number',
+              hintText: 'Enter your phone number',
+              border: OutlineInputBorder(),
+            ),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter your phone number';
+              }
+              return null;
+            },
+          ),
+          SizedBox(height: 16),
+          TextFormField(
+            controller: _textController3,
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              labelText: 'Email address',
+              hintText: 'Enter your email address',
+              border: OutlineInputBorder(),
+            ),
+            validator: (value) {
+              if (value!.isEmpty) {
+                return 'Please enter your email address';
+              }
+              return null;
+            },
+          ),
+          SizedBox(height: 32),
+          ElevatedButton(
+            onPressed: _generateQRCode,
+            child: Text('Generate QR Code'),
+          ),
+          SizedBox(height: 16),
+          if (_qrData.isNotEmpty)
+            QrImage(
+              data: _qrData,
+              version: QrVersions.auto,
+              size: 200,
+            ),
+        ],
+      ),
+    );
+  }
+}
